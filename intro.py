daa3715715d67780797bd109fe24a7e37bc7c131
Victor Miranda <77183797+Pistoriomax3ro@users.noreply.github.com>
2026-05-28 17:59:57 -0500

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+
+age = input("How old are you? ")
+
+hobby = input("What is your favorite hobby? ")
+
+career = input ("What is your career? ")
+
+print(f"Hello, {name}! You are {age} years old, enjoy {hobby}, and work as a {career}.")
+
